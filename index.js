f2125e2ac64a24ec2f7751aa48157c752461ebeb
mzxwt <mzxwt@users.noreply.github.com>
2020-01-07 12:43:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const package = require('./package.json')
 const CIDR_PATH = './cn-aggregated.zone.txt';
 const TEMPLATE_PAC_PATH = './whitelist_template.pac';
 const DIST_PAC_PATH = './whitelist.pac';
-const PROXY = 'SOCKS localhost:1080';
+const PROXY = 'SOCKS localhost:1080; SOCKS5 localhost:1080;';
 
 const cidrsFile = fs.readFileSync(CIDR_PATH);
 const cidrs = cidrsFile.toString().split(/\n|\r\n/);
